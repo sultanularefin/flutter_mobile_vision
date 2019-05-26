514a23f542a38cebdf345b9f0f334af0d5c156a4
sultanularefin <sultanul.arefin@softbakso.com>
2019-05-26 13:33:21 +0600

    only barcode functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile_vision/flutter_mobile_vision.dart';
 import 'barcode_detail.dart';
-import 'face_detail.dart';
-import 'ocr_text_detail.dart';
-
 
 void main() => runApp(new BarCodeScanner());
 
@@ -14,7 +11,6 @@ class BarCodeScanner extends StatefulWidget {
 
   @override
   _BarCodeScannerState createState() => _BarCodeScannerState();
-
 
 }
 
@@ -31,30 +27,14 @@ class _BarCodeScannerState extends State<BarCodeScanner> {
   Size _previewBarcode;
   List<Barcode> _barcodes = [];
 
-  int _cameraOcr = FlutterMobileVision.CAMERA_BACK;
-  bool _autoFocusOcr = true;
-  bool _torchOcr = false;
-  bool _multipleOcr = false;
-  bool _waitTapOcr = false;
-  bool _showTextOcr = true;
-  Size _previewOcr;
-  List<OcrText> _textsOcr = [];
 
-  int _cameraFace = FlutterMobileVision.CAMERA_FRONT;
-  bool _autoFocusFace = true;
-  bool _torchFace = false;
-  bool _multipleFace = true;
-  bool _showTextFace = true;
-  Size _previewFace;
-  List<Face> _faces = [];
 
   @override
   void initState() {
     super.initState();
     FlutterMobileVision.start().then((previewSizes) => setState(() {
       _previewBarcode = previewSizes[_cameraBarcode].first;
-      _previewOcr = previewSizes[_cameraOcr].first;
-      _previewFace = previewSizes[_cameraFace].first;
+
     }));
   }
 
@@ -62,28 +42,22 @@ class _BarCodeScannerState extends State<BarCodeScanner> {
   Widget build(BuildContext context) {
     return new MaterialApp(
       theme: new ThemeData(
-        primarySwatch: Colors.lime,
-        buttonColor: Colors.lime,
+        primarySwatch: Colors.blue,
+        buttonColor: Colors.blue,
       ),
       home: new DefaultTabController(
         length: 3,
         child: new Scaffold(
           appBar: new AppBar(
-            bottom: new TabBar(
-              indicatorColor: Colors.black54,
-              tabs: [
-                new Tab(text: 'Barcode'),
-                new Tab(text: 'OCR'),
-                new Tab(text: 'Face')
-              ],
-            ),
-            title: new Text('Flutter Mobile Vision'),
+
+            title: new Text('Inventory App'),
+              centerTitle: true
           ),
-          body: new TabBarView(children: [
-            _getBarcodeScreen(context),
-            _getOcrScreen(context),
-            _getFaceScreen(context),
-          ]),
+
+
+          body: Center(
+            child: _getBarcodeScreen(context),
+          ),
         ),
       ),
     );
@@ -133,6 +107,8 @@ class _BarCodeScannerState extends State<BarCodeScanner> {
     List<DropdownMenuItem<Size>> previewItems = [];
 
     List<Size> sizes = FlutterMobileVision.getPreviewSizes(facing);
+
+//    print ('sizes are $sizes');
 
     if (sizes != null) {
       sizes.forEach((size) {
@@ -275,8 +251,12 @@ class _BarCodeScannerState extends State<BarCodeScanner> {
           bottom: 12.0,
         ),
         child: new RaisedButton(
+
+
           onPressed: _scan,
           child: new Text('SCAN!'),
+
+
         ),
       ),
     );
@@ -326,280 +306,6 @@ class _BarCodeScannerState extends State<BarCodeScanner> {
     setState(() => _barcodes = barcodes);
   }
 
-  ///
-  /// OCR Screen
-  ///
-  Widget _getOcrScreen(BuildContext context) {
-    List<Widget> items = [];
-
-    items.add(new Padding(
-      padding: const EdgeInsets.only(
-        top: 8.0,
-        left: 18.0,
-        right: 18.0,
-      ),
-      child: const Text('Camera:'),
-    ));
-
-    items.add(new Padding(
-      padding: const EdgeInsets.only(
-        left: 18.0,
-        right: 18.0,
-      ),
-      child: new DropdownButton(
-        items: _getCameras(),
-        onChanged: (value) {
-          _previewOcr = null;
-          setState(() => _cameraOcr = value);
-        },
-        value: _cameraOcr,
-      ),
-    ));
-
-    items.add(new Padding(
-      padding: const EdgeInsets.only(
-        top: 8.0,
-        left: 18.0,
-        right: 18.0,
-      ),
-      child: const Text('Preview size:'),
-    ));
-
-    items.add(new Padding(
-      padding: const EdgeInsets.only(
-        left: 18.0,
-        right: 18.0,
-      ),
-      child: new DropdownButton(
-        items: _getPreviewSizes(_cameraOcr),
-        onChanged: (value) {
-          setState(() => _previewOcr = value);
-        },
-        value: _previewOcr,
-      ),
-    ));
-
-    items.add(new SwitchListTile(
-      title: const Text('Auto focus:'),
-      value: _autoFocusOcr,
-      onChanged: (value) => setState(() => _autoFocusOcr = value),
-    ));
-
-    items.add(new SwitchListTile(
-      title: const Text('Torch:'),
-      value: _torchOcr,
-      onChanged: (value) => setState(() => _torchOcr = value),
-    ));
-
-    items.add(new SwitchListTile(
-      title: const Text('Return all texts:'),
-      value: _multipleOcr,
-      onChanged: (value) => setState(() => _multipleOcr = value),
-    ));
-
-    items.add(new SwitchListTile(
-      title: const Text('Capture when tap screen:'),
-      value: _waitTapOcr,
-      onChanged: (value) => setState(() => _waitTapOcr = value),
-    ));
-
-    items.add(new SwitchListTile(
-      title: const Text('Show text:'),
-      value: _showTextOcr,
-      onChanged: (value) => setState(() => _showTextOcr = value),
-    ));
-
-    items.add(
-      new Padding(
-        padding: const EdgeInsets.only(
-          left: 18.0,
-          right: 18.0,
-          bottom: 12.0,
-        ),
-        child: new RaisedButton(
-          onPressed: _read,
-          child: new Text('READ!'),
-        ),
-      ),
-    );
-
-    items.addAll(
-      ListTile.divideTiles(
-        context: context,
-        tiles: _textsOcr
-            .map(
-              (ocrText) => new OcrTextWidget(ocrText),
-        )
-            .toList(),
-      ),
-    );
-
-    return new ListView(
-      padding: const EdgeInsets.only(
-        top: 12.0,
-      ),
-      children: items,
-    );
-  }
-
-  ///
-  /// OCR Method
-  ///
-  Future<Null> _read() async {
-    List<OcrText> texts = [];
-    try {
-      texts = await FlutterMobileVision.read(
-        flash: _torchOcr,
-        autoFocus: _autoFocusOcr,
-        multiple: _multipleOcr,
-        waitTap: _waitTapOcr,
-        showText: _showTextOcr,
-        preview: _previewOcr,
-        camera: _cameraOcr,
-        fps: 2.0,
-      );
-    } on Exception {
-      texts.add(new OcrText('Failed to recognize text.'));
-    }
-
-    if (!mounted) return;
-
-    setState(() => _textsOcr = texts);
-  }
-
-  ///
-  /// Face Screen
-  ///
-  Widget _getFaceScreen(BuildContext context) {
-    List<Widget> items = [];
-
-    items.add(new Padding(
-      padding: const EdgeInsets.only(
-        top: 8.0,
-        left: 18.0,
-        right: 18.0,
-      ),
-      child: const Text('Camera:'),
-    ));
-
-    items.add(new Padding(
-      padding: const EdgeInsets.only(
-        left: 18.0,
-        right: 18.0,
-      ),
-      child: new DropdownButton(
-        items: _getCameras(),
-        onChanged: (value) {
-          _previewFace = null;
-          setState(() => _cameraFace = value);
-        },
-        value: _cameraFace,
-      ),
-    ));
-
-    items.add(new Padding(
-      padding: const EdgeInsets.only(
-        top: 8.0,
-        left: 18.0,
-        right: 18.0,
-      ),
-      child: const Text('Preview size:'),
-    ));
-
-    items.add(new Padding(
-      padding: const EdgeInsets.only(
-        left: 18.0,
-        right: 18.0,
-      ),
-      child: new DropdownButton(
-        items: _getPreviewSizes(_cameraFace),
-        onChanged: (value) {
-          setState(() => _previewFace = value);
-        },
-        value: _previewFace,
-      ),
-    ));
-
-    items.add(new SwitchListTile(
-      title: const Text('Auto focus:'),
-      value: _autoFocusFace,
-      onChanged: (value) => setState(() => _autoFocusFace = value),
-    ));
-
-    items.add(new SwitchListTile(
-      title: const Text('Torch:'),
-      value: _torchFace,
-      onChanged: (value) => setState(() => _torchFace = value),
-    ));
-
-    items.add(new SwitchListTile(
-      title: const Text('Multiple:'),
-      value: _multipleFace,
-      onChanged: (value) => setState(() => _multipleFace = value),
-    ));
-
-    items.add(new SwitchListTile(
-      title: const Text('Show text:'),
-      value: _showTextFace,
-      onChanged: (value) => setState(() => _showTextFace = value),
-    ));
-
-    items.add(
-      new Padding(
-        padding: const EdgeInsets.only(
-          left: 18.0,
-          right: 18.0,
-          bottom: 12.0,
-        ),
-        child: new RaisedButton(
-          onPressed: _face,
-          child: new Text('DETECT!'),
-        ),
-      ),
-    );
-
-    items.addAll(
-      ListTile.divideTiles(
-        context: context,
-        tiles: _faces
-            .map(
-              (face) => new FaceWidget(face),
-        )
-            .toList(),
-      ),
-    );
-
-    return new ListView(
-      padding: const EdgeInsets.only(
-        top: 12.0,
-      ),
-      children: items,
-    );
-  }
-
-  ///
-  /// Face Method
-  ///
-  Future<Null> _face() async {
-    List<Face> faces = [];
-    try {
-      faces = await FlutterMobileVision.face(
-        flash: _torchFace,
-        autoFocus: _autoFocusFace,
-        multiple: _multipleFace,
-        showText: _showTextFace,
-        preview: _previewFace,
-        camera: _cameraFace,
-        fps: 15.0,
-      );
-    } on Exception {
-      faces.add(new Face(-1));
-    }
-
-    if (!mounted) return;
-
-    setState(() => _faces = faces);
-  }
 }
 
 ///
@@ -623,53 +329,6 @@ class BarcodeWidget extends StatelessWidget {
       onTap: () => Navigator.of(context).push(
         new MaterialPageRoute(
           builder: (context) => new BarcodeDetail(barcode),
-        ),
-      ),
-    );
-  }
-}
-
-///
-/// OcrTextWidget
-///
-class OcrTextWidget extends StatelessWidget {
-  final OcrText ocrText;
-
-  OcrTextWidget(this.ocrText);
-
-  @override
-  Widget build(BuildContext context) {
-    return new ListTile(
-      leading: const Icon(Icons.title),
-      title: new Text(ocrText.value),
-      subtitle: new Text(ocrText.language),
-      trailing: const Icon(Icons.arrow_forward),
-      onTap: () => Navigator.of(context).push(
-        new MaterialPageRoute(
-          builder: (context) => new OcrTextDetail(ocrText),
-        ),
-      ),
-    );
-  }
-}
-
-///
-/// FaceWidget
-///
-class FaceWidget extends StatelessWidget {
-  final Face face;
-
-  FaceWidget(this.face);
-
-  @override
-  Widget build(BuildContext context) {
-    return new ListTile(
-      leading: const Icon(Icons.face),
-      title: new Text(face.id.toString()),
-      trailing: const Icon(Icons.arrow_forward),
-      onTap: () => Navigator.of(context).push(
-        new MaterialPageRoute(
-          builder: (context) => new FaceDetail(face),
         ),
       ),
     );
